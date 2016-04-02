8f85e10056729858f41f50761530bb62d78d5188
Martin Jönsson <nathaniel@endemoniada.org>
2016-04-02 14:33:13 +0200

    Improving battery info in tmux

--- getbatt.sh
+++ getbatt.sh
@@ -4,18 +4,20 @@
 battpercent=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $1 }' | awk -F' ' '{ print $2 }')
 
 # Get power source
-powersource=$(pmset -g batt | grep 'Now drawing from' | awk -F"'" '{ print $2 }')
+powersource=$(pmset -g batt | grep 'Now drawing from' | awk -F"'" '{ print $2 }' | awk -F' ' '{ print $1 }' | head -c 4)
 
 # Get battery status
 battstatus=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $2 }' | awk -F' ' '{ print $1 }')
 
 # Get remaining charge/battery time
-remtime=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $3 }' | awk -F' ' '{ print $1" "$2 }')
+remtime=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $3 }' | awk -F' ' '{ print $1 }')
 
-echo -n "$powersource $battpercent"
-if [[ $battstatus != "charged" ]]; then
-	echo " $remtime"
-else
-	echo ""
+echo -n "$powersource"
+if [[ $battpercent != "100%" ]]; then
+	echo -n " $battpercent"
 fi
+if [[ $battstatus != "charged" ]]; then
+	echo -n " $remtime"
+fi
+echo ""
 exit 0
