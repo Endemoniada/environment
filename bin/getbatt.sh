d2074fcdf05fd7ad5892a3066b6bba6d1b770f7a
Martin Jönsson <nathaniel@endemoniada.org>
2016-10-24 16:55:52 +0200

    Fix for Sierra

--- bin/getbatt.sh
+++ bin/getbatt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get battery percentage
-battpercent=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $1 }' | awk -F' ' '{ print $2 }')
+battpercent=$(pmset -g batt | grep 'Battery-0' | awk -F';' '{ print $1 }' | awk -F' ' '{ print $3 }')
 
 # Get power source
 powersource=$(pmset -g batt | grep 'Now drawing from' | awk -F"'" '{ print $2 }' | awk -F' ' '{ print $1 }' | head -c 4)
