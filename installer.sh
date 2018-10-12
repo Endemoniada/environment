fb879910c01bde9c13dcdb637e1d4db2e2db6bcf
Martin Ekberg <martin@mekberg.se>
2018-10-12 13:25:26 +0200

    Some refactoring of install script

--- installer.sh
+++ installer.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Make sure we exit as soon as something goes wrong
+set -e
+
 source_dir=$(dirname $0)
+sys_bin_dir=$sys_bin_dir
+sys_sysd_dir=$sys_sysd_dir
+sys_etc_dir=$sys_etc_dir
 kernel=$(uname)
 mail=$1
 
+# Usage: `ask [Y|N]`
+# Supplied parameter will decide the default answer
+# Default is No`
 ask () {
   default=$(echo ${1:0:1} | awk '{print toupper($0)}')
   if [[ "$default" == "Y" ]]; then
@@ -18,6 +27,7 @@ ask () {
   echo $(echo ${answer:0:1} | awk '{print toupper($0)}')
 }
 
+# Walk through and install user dotfiles in main directory
 echo
 echo "Do you want to install user dotfiles?"
 if [[ "$(ask Y)" == "Y" ]]; then
@@ -54,20 +64,19 @@ if [[ "$(ask Y)" == "Y" ]]; then
   done
 fi
 
-# Install backup script and necessary files
-dist=$(sed -n "s/^ID=\(.*\)$/\1/p" /etc/*-release)
-if [[ "$dist" == "arch" || "$dist" == "archarm" ]]; then
+# Install system files in /system/ directory
+if [[ "$(hostname)" == "archivist" || "$(hostname)" == "archon" ]]; then
   echo
   echo "Do you want to install system binaries, system config files"
   echo "and install and enable systemd units/timers? (will invoke sudo)"
   if [[ "$(ask N)" == "Y" ]]; then
     echo
     # Install custom scripts to /usr/local/bin/
-    for file in $(find $source_dir/system/bin -maxdepth 1 -type f | grep -v -i readme); do
+    for file in $(find $sys_bin_dir -maxdepth 1 -type f | grep -v -i readme); do
       sudo cp -v $file /usr/local/bin/
     done
     # Install config files to /etc/ (does not overwrite existing files)
-    for file in $(find $source_dir/system/etc -maxdepth 1 -type f | grep -v -i readme); do
+    for file in $(find $sys_etc_dir -maxdepth 1 -type f | grep -v -i readme); do
       if [ -r /etc/${file} ]; then
         echo "File /etc/${file} already exists, will not overwrite"
       else
@@ -75,25 +84,14 @@ if [[ "$dist" == "arch" || "$dist" == "archarm" ]]; then
       fi
     done
     # Install service unit files and enable them
-    for file in $(find $source_dir/system/systemd -maxdepth 1 -type f -iname '*.service' | grep -v -i readme); do
+    for file in $(find $sys_sysd_dir -maxdepth 1 -type f -iname '*.service' | grep -v -i readme); do
       sudo cp -v $file /etc/systemd/system/ && sudo systemctl daemon-reload
       grep -q -i 'oneshot' $file || sudo systemctl enable $(basename $file)
     done
-    if [[ -d $source_dir/system/systemd/$(hostname) ]]; then
-      for file in $(find $source_dir/system/systemd/$(hostname) -maxdepth 1 -type f -iname '*.service' | grep -v -i readme); do
-        sudo cp -v $file /etc/systemd/system/ && sudo systemctl daemon-reload
-        grep -q -i 'oneshot' $file || sudo systemctl enable $(basename $file)
-      done
-    fi
     # Install timer unit files and enable them
-    for file in $(find $source_dir/system/systemd -maxdepth 1 -type f -iname '*.timer' | grep -v -i readme); do
+    for file in $(find $sys_sysd_dir -maxdepth 1 -type f -iname '*.timer' | grep -v -i readme); do
       sudo cp -v $file /etc/systemd/system/ && sudo systemctl daemon-reload && sudo systemctl enable $(basename $file)
     done
-    if [[ -d $source_dir/system/systemd/$(hostname) ]]; then
-      for file in $(find $source_dir/system/systemd/$(hostname) -maxdepth 1 -type f -iname '*.timer' | grep -v -i readme); do
-        sudo cp -v $file /etc/systemd/system/ && sudo systemctl daemon-reload && sudo systemctl enable $(basename $file)
-      done
-    fi
   fi
 fi
 
