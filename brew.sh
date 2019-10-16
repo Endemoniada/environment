8c360d7a7c3137c5fcdc97d9a1648a97f9f2b12e
Martin Ekberg <martin@mekberg.se>
2019-10-16 09:19:57 +0200

    Add more applications to brew install

--- brew.sh
+++ brew.sh
@@ -2,7 +2,18 @@
 
 brew install python
 
+brew cask install 1password
+brew cask install atom
+brew cask install bettertouchtool
+brew cask install daisydisk
+brew cask install discord
 brew cask install divvy
+brew cask install firefox
+brew cask install istat-menus
+brew cask install keepingyouawake
+brew cask install scroll-reverser
+brew cask install telegram
+brew cask install the-unarchiver
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
